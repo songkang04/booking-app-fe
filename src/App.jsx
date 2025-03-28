c9de399f35565d157713c80075110dd4032564e5
Danh Hùng Phan <danhhungphan@Danhs-MacBook-Pro.local>
2025-03-28 22:23:03 +0700

    init proẹct

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import './App.css';
+import { Home, Login, NotFound, Register } from './pages';
+
+function App() {
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/register" element={<Register />} />
+        <Route path="*" element={<NotFound />} />
+      </Routes>
+    </BrowserRouter>
+  );
+}
+
+export default App;
